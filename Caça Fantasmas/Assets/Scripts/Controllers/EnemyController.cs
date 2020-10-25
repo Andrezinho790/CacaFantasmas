13f56cc785367f80944fc3351a8cfafca35899fd
Delacroz <andremenezes970@gmail.com>
2020-10-24 23:16:36 -0300

    Alguns Scripts

--- Caça Fantasmas/Assets/Scripts/Controllers/EnemyController.cs
+++ Caça Fantasmas/Assets/Scripts/Controllers/EnemyController.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public float lookRadius = 10f;
+    GameObject[] itensArremessaveis;
+    public Transform target;
+    NavMeshAgent agent;
+    bool EstaArremessando;
+    int randomNumber;
     // Start is called before the first frame update
     void Start()
     {
-        
+        EstaArremessando = false;
+        itensArremessaveis = GameObject.FindGameObjectsWithTag("Arremessaveis");
+        //target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+
+         ArremessarItem();
         
+        
+        
+
+        
+    }
+
+    void ArremessarItem()
+    {
+        
+        if (!EstaArremessando)
+        {
+            randomNumber = Random.Range(0, itensArremessaveis.Length);
+        }
+        if (itensArremessaveis[randomNumber] != null)
+        {
+            Debug.Log(itensArremessaveis[randomNumber]);
+            EstaArremessando = true;
+            agent.SetDestination(itensArremessaveis[randomNumber].transform.position);
+        }
     }
 }
